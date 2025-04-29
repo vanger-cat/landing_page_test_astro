6361ec3f57dfb9e1019a8669a59b4caa12408628
Anton Katkov <katkov.anton@gmail.com>
2025-04-29 14:43:01 +0200

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  base: "/landing_page_test_astro/", // replace with actual repo
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
